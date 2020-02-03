b279a1b019d77bf2e4b3af0015a6099a28c1b901
Daphne <daphne.feresin@gmail.com>
2020-02-03 03:15:10 -0300

    final changes

--- src/index.js
+++ src/index.js
@@ -40,4 +40,28 @@ document.getElementById("contact-form").onclick = function() {
 document.getElementById("my-form").submit();
 document.getElementById("success-form").innerHTML ="Your message has been successfully sent.";
 }
+//................................................................................
+//......headline change on Logo click.............................................
+let headlines = ["Delicious cakes!","Amazing cakes!","Yummy cakes!", "Tasty cakes!"]
 
+document.getElementById("logo").onclick = function() {
+  let randomHeadline = headlines[Math.floor(Math.random() * headlines.length)]
+  document.getElementById("headline").innerHTML = randomHeadline;
+}
+//..............................................................................
+//Text with 50 words changes by choosing a specific URL parameter, with fallback.
+//Parameter = text
+window.onload = function() {
+  try {
+      let url_string = (window.location.href).toLowerCase();
+      let url = new URL(url_string);
+      let text = url.searchParams.get("text");
+      if(!text){
+//default text if no parameter:
+        text="We are a small bakery, specializing in custom cakes for any special occasion. We believe firmly in baking fresh and from scratch each day, using only the best and local ingredients whenever possible. Check out our full list of services, and feel free to contact us if you have any questions."
+      }
+      document.getElementById("50words").innerHTML = text;
+  } catch (err) {
+      console.log("Issues with Parsing URL Parameter's - " + err);
+  }
+}
